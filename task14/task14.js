664aa868d335e278e466f25bf98d407d38e99f07
Vadim Khomenkov <maestrik007@yandex.ru>
2020-02-17 20:07:01 +0300

    task14

---
+++ task14/task14.js
@@ -0,0 +1,8 @@
+/*Реализуйте функцию stepByStep(num1,num2,step)*/
+const stepByStep = (num1, num2, step) => {
+    let tmp = [];
+    for (let i = 0; i <= step; i++)
+        tmp[i] = num1 + i * (num2 - num1) / step;
+    return tmp;
+}
+console.log(stepByStep(20, 40, 5));
