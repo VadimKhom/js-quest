5a6c312f6c7d547205285417ca1c65f8b0b78628
Vadim Khomenkov <maestrik007@yandex.ru>
2020-02-18 17:44:02 +0300

    task16

---
+++ task16/task16.js
@@ -0,0 +1,6 @@
+/*Реализуйте функцию randRange(n1,n2)*/
+const randRange = (n1, n2) => Math.round(Math.random() * (n2 - n1)) + n1;
+console.clear();
+console.log(randRange(0, 100));
+console.log(randRange(0, 100));
+console.log(randRange(0, 100));
