317dab1f52b18c7583b1c9433d36cd2208746431
Vadim Khomenkov <maestrik007@yandex.ru>
2020-02-19 13:51:07 +0300

    task18

---
+++ task18/task18.js
@@ -0,0 +1,21 @@
+/*Реализуйте функцию category(categories)*/
+const category = cat => {
+    let res = "";
+    do res += cat.category + "/";
+    while ((cat = cat.next));
+    return res.slice(0, -1);
+};
+const categories = {
+    category: "Компьютеры",
+    next: {
+        category: "Ноутбуки",
+        next: {
+            category: "Все ноутбуки",
+            next: {
+                category: "Игровые",
+                next: null
+            }
+        }
+    }
+};
+console.log(category(categories));
